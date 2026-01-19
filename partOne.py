d648c1f61d824a79b38972b5ee5eca78637b4241
kem47 <kem47@kent.ac.uk>
2026-01-19 13:38:31 +0000

    whsh

--- partOne.py
+++ partOne.py
@@ -3,7 +3,7 @@ def main():
     myFunction(slow) #refer to myFunction function and complete task
     
 def myFunction(text): #text instaed of slow as to not confuse program, text is just saying whatever information incoming
-    print(f"Output: {text.replace(" ", "...")}") # formatt is str.replace(old, new) and print(f"") added here so we can see reults
+    print(f"Output: {text.replace(" ", "...")}") # format is str.replace(old, new) and print(f"") added here so we can see reults
 main()
 
 
