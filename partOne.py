59605aeb25ca1e60b0a32544acef87febb39be7b
kem47 <kem47@kent.ac.uk>
2026-01-19 13:37:01 +0000

    extra

--- partOne.py
+++ partOne.py
@@ -6,4 +6,4 @@ def myFunction(text): #text instaed of slow as to not confuse program, text is j
     print(f"Output: {text.replace(" ", "...")}") # formatt is str.replace(old, new) and print(f"") added here so we can see reults
 main()
 
-
+remove extra
