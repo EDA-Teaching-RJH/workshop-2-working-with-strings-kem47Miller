3336e11164a4f178d4ac58ea49a5c372464f9a21
kem47 <kem47@kent.ac.uk>
2026-01-19 13:23:20 +0000

    more of the same

---
+++ workshop2sect2 if-else.py
@@ -0,0 +1,17 @@
+import random
+
+def num(t, p):
+    if t > p:
+        print("Number too high")
+    elif t == p:
+        print("Correct!")
+    else:
+        print("Number too low")
+         
+def main():
+    guess = int(input("Guess a number from 1 to 10: "))
+    secret_number = random.randint(1, 10)
+    num(guess, secret_number)
+    print(secret_number)
+
+main()  
