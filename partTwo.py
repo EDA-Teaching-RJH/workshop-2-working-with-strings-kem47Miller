e2ae4d55f0616e53ff8c4a19a242c6f29266c232
kem47 <kem47@kent.ac.uk>
2026-01-19 13:14:06 +0000

    workshop2part2

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,25 @@
-import math  
-
+import math
 def main():
-#TO DO  
-
+    num1=int(input("A: "))
+    num2=int(input("B: "))
+    print(pythag(num1,num2))
+    #alternatives:
+    #C=(pythag(num1,num2))
+    #print(C)
+    
+#TO DO
 def pythag(A,B):
+    C=math.sqrt(A**2 + B**2)
+    return C
+#TO DO
+main()
+
+#import math  
+
+#def main():
 #TO DO  
 
-main()
+#def pythag(A,B):
+#TO DO  
+
+#main()
