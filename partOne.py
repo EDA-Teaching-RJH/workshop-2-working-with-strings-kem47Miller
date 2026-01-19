28711d22f7dbb523519afa7689ec2de99d7c9ced
kem47 <kem47@kent.ac.uk>
2026-01-19 13:12:04 +0000

    workshop2part1

--- partOne.py
+++ partOne.py
@@ -1,8 +1,16 @@
 def main():
-    slow = input("Input ")
-    myFunction(slow)
+    slow = input("Input: ") #ask for input stores it in slow
+    myFunction(slow) #refer to myFunction function and complete task
+    
+def myFunction(text): #text instaed of slow as to not confuse program, text is just saying whatever information incoming
+    print(f"Output: {text.replace(" ", "...")}") # formatt is       str.replace(old, new) and print(f"") added here so we can see reults
+main()
 
-def myFunction(text):
+#def main():
+    #slow = input("Input ")
+    #myFunction(slow)
+
+#def myFunction(text):
   #Your code goes here.
 
-main()
+#main()
